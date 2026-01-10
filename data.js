17ea401056811206139430e02e4d2fa5a95ab846
xaqec <162811352+xaqec@users.noreply.github.com>
2026-01-10 17:01:07 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const siteData = {
+    title: "İÇ DÖKME ALANI",
+    placeholder: "Seni üzen, canını sıkan veya unutmak istediğin ne varsa buraya yaz...",
+    burnButtonText: "🔥 YAK VE KURTUL",
+    ashMessage: "Küller rüzgara karıştı.\nGeçmiş olsun.",
+    alertEmpty: "Yakacak bir şey yazmadın."
+};
